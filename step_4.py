842998e252022d0a0da9effcc703368d5b2b3262
Emilio Aurea <emilioaurea@gmail.com>
2016-08-18 11:52:55 -0700

    not sure why not passing step_4 still

--- step_4.py
+++ step_4.py
@@ -11,7 +11,3 @@ words_without_prefix = [word for word in words if not word[:prefix_size] == pref
 r = requests.post('http://challenge.code2040.org/api/prefix/validate',
 data = {'token': '38435f83ebfecb02e00892e83c22862f',
         'array': words_without_prefix})
-print(words)
-print(prefix)
-print(words_without_prefix)
-print(r.text)
