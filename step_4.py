285cf60f5fecb1079428e9e350b0c3918f58b1c5
Emilio Aurea <emilioaurea@gmail.com>
2016-08-18 11:51:11 -0700

    not sure why not passing step_4

--- step_4.py
+++ step_4.py
@@ -6,9 +6,12 @@ data = {'token': '38435f83ebfecb02e00892e83c22862f'})
 content = response.json()
 prefix, words = content['prefix'], content['array']
 prefix_size = len(prefix)
-words_without_prefix = [word for word in words if word[:prefix_size] != prefix]
-
+words_without_prefix = [word for word in words if not word[:prefix_size] == prefix]
 
 r = requests.post('http://challenge.code2040.org/api/prefix/validate',
 data = {'token': '38435f83ebfecb02e00892e83c22862f',
         'array': words_without_prefix})
+print(words)
+print(prefix)
+print(words_without_prefix)
+print(r.text)
