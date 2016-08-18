94346b9d5c97149585a79104df6b4a932f0f6cf0
Emilio Aurea <emilioaurea@gmail.com>
2016-08-17 23:20:30 -0700

    debugging step 4. looks right tho

---
+++ step_4.py
@@ -0,0 +1,14 @@
+import requests
+
+response = requests.post('http://challenge.code2040.org/api/prefix',
+data = {'token': '38435f83ebfecb02e00892e83c22862f'})
+
+content = response.json()
+prefix, words = content['prefix'], content['array']
+prefix_size = len(prefix)
+words_without_prefix = [word for word in words if word[:prefix_size] != prefix]
+
+
+r = requests.post('http://challenge.code2040.org/api/prefix/validate',
+data = {'token': '38435f83ebfecb02e00892e83c22862f',
+        'array': words_without_prefix})
